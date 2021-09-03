7c22064e4248f0c289fe28f21ca69796b8d4ebd5
Casey Law <caseyjlaw@gmail.com>
2021-09-03 12:00:37 -0700

    fixing get_coords to be more general. fiing docstring

--- psquery/__init__.py
+++ psquery/__init__.py
@@ -14,15 +14,18 @@ def get_radec(*args):
             co = coordinates.SkyCoord(ra=ra0, dec=dec0, unit=units.deg)
 
     elif len(args) == 1:
-        try:
-            ra0, dec0 = args[0].split(',')
-        except ValueError:
-            ra0, dec0 = args[0].split(' ')
+        if not isinstance(args[0], coordinates.SkyCoord):
+            try:
+                ra0, dec0 = args[0].split(',')
+            except ValueError:
+                ra0, dec0 = args[0].split(' ')
             
-        if ':' in ra0:
-            co = coordinates.SkyCoord(ra=ra0, dec=dec0, unit=(units.hourangle, units.deg))
+            if ':' in ra0:
+                co = coordinates.SkyCoord(ra=ra0, dec=dec0, unit=(units.hourangle, units.deg))
+            else:
+                co = coordinates.SkyCoord(ra=ra0, dec=dec0, unit=units.deg)
         else:
-            co = coordinates.SkyCoord(ra=ra0, dec=dec0, unit=units.deg)
+            co = args[0]
     else:
         print('could not parse args: ', args)
 
--- psquery/casdaquery.py
+++ psquery/casdaquery.py
@@ -1,11 +1,13 @@
 from astroquery.utils.tap.core import TapPlus
 from . import get_radec
-def cone_racs(radec, radius=5., selectcol=['id', 'ra_deg_cont', 'dec_deg_cont', 'ra_err',
+
+
+def cone_racs(radec, radius=5/3600, selectcol=['id', 'ra_deg_cont', 'dec_deg_cont', 'ra_err',
                                              'dec_err', 'flux_peak', 'flux_peak_err', 'flux_int',
                                              'flux_int_err', 'maj_axis', 'min_axis']):
     """ cone search of ASKAP/RACS.
     ra, dec in any format (parsed by get_radec).
-    radius in arcsec.
+    radius in degrees.
     """
 
     ra, dec = get_radec(radec)
@@ -13,7 +15,7 @@ def cone_racs(radec, radius=5., selectcol=['id', 'ra_deg_cont', 'dec_deg_cont', 
 #    base = 'http://casda.csiro.au/casda_vo_tools/tap/sync?request=doQuery&lang=ADQL&format=csv&query='
     casdatap = TapPlus(url="https://casda.csiro.au/casda_vo_tools/tap")
 
-    query = f"SELECT * FROM casda.continuum_component where 1=CONTAINS(POINT('ICRS', ra_deg_cont, dec_deg_cont),CIRCLE('ICRS',{ra},{dec},{radius})) and project_id = 23" 
+    query = f"SELECT * FROM casda.continuum_component where 1=CONTAINS(POINT('ICRS', ra_deg_cont, dec_deg_cont), CIRCLE('ICRS', {ra}, {dec}, {radius})) and project_id = 23" 
 
 #    tab = ascii.read(data.decode('utf-8'))
     tab = casdatap.launch_job(query).get_results()
