359fed0ae676768a3ae5a5c3898762947e7d085d
Casey Law <caseyjlaw@gmail.com>
2022-02-12 06:32:31 -0800

    bump version. add requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 from setuptools import setup
 
 setup(name='psquery',
-      version='0.5.0',
+      version='0.6.0',
       url='http://github.com/realfastvla/psquery',
       packages=['psquery'],
+      install_requies=[
+          'astropy',
+          'extinction',
+          'sedpy',
+          'dustmaps',
+          'mastcasjobs',
+          'astroquery',
+          'pyvo'
+      ],
       zip_safe=False)
