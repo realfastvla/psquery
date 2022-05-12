164a80d4dc81226f2232938ac6748b6400dc07c3
Casey Law <caseyjlaw@gmail.com>
2022-05-12 10:21:29 -0700

    can run cone search on vlass catalogs in ascii format

--- psquery/catquery.py
+++ psquery/catquery.py
@@ -1,6 +1,8 @@
-from astropy.io import ascii
+import numpy as np
+
+from astropy import table
 from astropy import coordinates
-from astropy import units as u
+from astropy import units
 
 from . import get_coord
 
@@ -16,24 +18,43 @@ def parse_cat(filename):
     - all_e2_gtr_1mJy.csv
     """
 
-    pass
+    if "CIRADA" in filename:
+        colnames = ['Component_name', 'RA', 'DEC', 'E_RA', 'E_DEC', 'Total_flux', 'E_Total_flux', 'Peak_flux', 'E_Peak_flux', 'Maj', 'E_Maj', 'Min', 'E_Min', 'PA', 'E_PA', 'Tile', 'NVSS_distance', 'Peak_to_ring', 'Duplicate_flag', 'Quality_flag']
+    else:
+        colnames = None
+
+    tab = table.Table.read(filename, guess=False, format="csv", include_names=colnames)
+
+    if "CIRADA" in filename:
+        tab.rename_column('Component_name', 'vlassname')
+        tab.rename_column("RA", "ra")
+        tab.rename_column("DEC", "dec")
+        sel = (tab['Duplicate_flag'] < 2) & ((tab['Quality_flag'] == 0) | (tab['Quality_flag'] == 4))
+        return tab[sel]
+    else:
+        return tab
 
 
-def cone_vlass(radec, radius=2, tab=None, filename=''):
+def cone_vlass(radec, radius=2, tab=None, filename='/Users/claw/data/vlass/CIRADA_VLASS1QL_table1_components_v2.csv'):
     """ Run cone search for radec tuple on vlass catalogs.
     Assumes vlass columns in astropy table format.
     radec tuple for coordinate.
     radius in arcseconds.
     """
 
-    if tab is None and filename is not None:
-        tab = parse_cat(filename=filename)
+    if tab is None:
+        if filename is not None:
+            tab = parse_cat(filename=filename)
+        else:
+            print("Must provide tab or filename")
+
+    assert isinstance(tab, table.Table)
 
     co = get_coord(radec, ret="skycoord")
-    cat = coordinates.SkyCoord(tab['ra'], tab['dec'], unit=u.deg)
+    cat = coordinates.SkyCoord(tab['ra'], tab['dec'], unit=units.deg)
 
-    sep = cat.separation(co).to_value(units.arcsec)
+    seps = cat.separation(co)
     sel = np.where(seps < radius*units.arcsec)[0]
 
-    return cat[sel]
+    return tab[sel]
 
