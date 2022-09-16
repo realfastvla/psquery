ea39d36c6508e23d8b869e48ae5c146ffac7a685
Casey Law <caseyjlaw@gmail.com>
2022-09-16 07:34:39 -0700

    update for csc21

--- psquery/chandraquery.py
+++ psquery/chandraquery.py
@@ -9,12 +9,14 @@ import pyvo
 from . import get_coord
 
 
-def cone_chandra(radec, radius=5):
+def cone_chandra(radec, radius=5, path="csc21_snapshot_scs"):
     """Query Chandra Source Catalog 2 with pyvo.
     radius in arcseconds.
+    path is portion of VO cone search url (e.g., https://cda.cfa.harvard.edu/{path}/coneSearch)
     """
 
-    url = "http://cda.cfa.harvard.edu/csc2scs/coneSearch"
+    oldpath = "csc2scs"
+    url = f"https://cda.cfa.harvard.edu/{path}/coneSearch"
     co = get_coord(radec, ret="skycoord")
     tab = pyvo.conesearch(url, co, radius=radius/3600).to_table()
 
